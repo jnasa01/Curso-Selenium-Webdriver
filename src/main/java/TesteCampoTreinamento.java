ec64d81de5a4848f9746700ee292061eeaf30549
Jairo Nascimento <jnasa01@hotmail.com>
2021-08-24 10:58:24 -0300

    Commit Inicial

---
+++ src/main/java/TesteCampoTreinamento.java
@@ -0,0 +1,43 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class TesteCampoTreinamento
+
+{
+
+    @Test
+    public void teste(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir")  + "/src/main/resources/componentes.html");
+        driver.quit();
+
+    }
+
+    @Test
+    public void testeTextField(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir")  + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste Inicial");
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Linha Complementar");
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        //driver.findElement(By.id("elementosForm:sexo:1")).click();
+        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+
+        driver.findElements(By.id("elementosForm:escolaridade") List("2grauincomp"));
+
+
+        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Área de texto derivada para comentários.");
+
+        //driver.quit();
+
+    }
+
+}
---
+++ src/main/java/TesteGoogle.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class TesteGoogle {
+
+    @Test
+    public void teste(){
+        //Setando as propriedades dos navegadores
+        //System.setProperties("webdriver.chrome.driver","C:\\Users\\jnasa\\Downloads\\geckodiver.exe" );
+        //System.setProperties("webdriver.chrome.driver","C:\\Users\\jnasa\\Downloads\\ChromeDriver.exe" );
+
+        //Abrindo instancia dos navegadores Chrome ou Firefox
+        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new InternetExplorerDriver(); // Validar configuração
+        //WebDriver driver = new EdgeDriver();// Validar configuração
+        //driver.manage().window().setSize(new Dimension(1200,765));
+        driver.manage().window().maximize();
+
+        driver.get("http://www.google.com");
+        //System.out.println(driver.getTitle());
+        Assert.assertEquals("Google", driver.getTitle());
+
+        driver.quit();
+
+    }
+
+}
