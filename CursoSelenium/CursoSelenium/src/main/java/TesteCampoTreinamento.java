4cd9eba69f68f8be2d217633c92e29d1b71afe36
Jairo Nascimento <jnasa01@hotmail.com>
2021-08-25 09:22:42 -0300

    Atualização no Git para pull
    Atualização no Git para pull

--- CursoSelenium/CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -9,7 +9,9 @@ public class TesteCampoTreinamento {
 	@Test
 	public void teste(){
 		WebDriver driver = new FirefoxDriver();
+		//Maximizando a tela do brownser
 		driver.manage().window().setSize(new Dimension(1200, 765));
+		//Acessando formulário componentes.html no diretório Resources do código selenium
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		
 
