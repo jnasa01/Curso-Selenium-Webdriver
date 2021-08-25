43365e6d247b2ebfdda6ab85dd2f2347a5923258
Jairo Nascimento <jnasa01@hotmail.com>
2021-08-25 13:59:05 -0300

    Seleção com RadioBUtton e Checkbox

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -13,14 +13,14 @@ public class TesteCampoTreinamento
 
     @Test
     public void teste(){
+        //Iniciando o ChromeDriver
         WebDriver driver = new ChromeDriver();
         //Maximizando a tela do bronwser
         driver.manage().window().maximize();
         // Abrindo arquivo componente.html no diretorio de resource do código
         driver.get("file:///" + System.getProperty("user.dir")  + "/src/main/resources/componentes.html");
-
         //fechando a sessão do brownser
-        //driver.quit();
+        driver.quit();
 
     }
 
@@ -30,25 +30,69 @@ public class TesteCampoTreinamento
         driver.manage().window().maximize();
         driver.get("file:///" + System.getProperty("user.dir")  + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste Inicial");
+        Assert.assertEquals("Teste Inicial", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
         driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Linha Complementar");
-        //driver.findElement(By.id("elementosForm:sexo:0")).click();
-        //driver.findElement(By.id("elementosForm:sexo:1")).click();
-        //driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+        Assert.assertEquals("Linha Complementar", driver.findElement(By.id("elementosForm:sobrenome")).getAttribute("value"));
 
-        //driver.findElements(By.id("elementosForm:escolaridade") List("2grauincomp")
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void deveInteragirComTextArea(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir")  + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Área de texto derivada para comentários.\n\nBy Jairo Nascimento");
+        Assert.assertEquals("Área de texto derivada para comentários.\n\nBy Jairo Nascimento", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
 
         //driver.quit();
 
     }
 
     @Test
-    public void testeTextArea(){
+    public void deveInteragirComRadioButton(){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("file:///" + System.getProperty("user.dir")  + "/src/main/resources/componentes.html");
-        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Área de texto derivada para comentários.");
 
-        //driver.quit();
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        //Validação com assertEquals e com AssertTrue
+//        Assert.assertEquals("M", driver.findElement(By.id("elementosForm:sexo:0")).getAttribute("value"));
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+
+
+//        driver.findElement(By.id("elementosForm:sexo:1")).click();
+//        //Validação com assertEquals e com AssertTrue
+//        Assert.assertEquals("F", driver.findElement(By.id("elementosForm:sexo:1")).getAttribute("value"));
+//        Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:1")).isSelected());
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void deveInteragirComCheckBox(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir")  + "/src/main/resources/componentes.html");
+
+//        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+//        Assert.assertEquals("carne", driver.findElement(By.id("elementosForm:comidaFavorita:0")).getAttribute("value"));
+
+//        driver.findElement(By.id("elementosForm:comidaFavorita:1")).click();
+//        Assert.assertEquals("frango", driver.findElement(By.id("elementosForm:comidaFavorita:1")).getAttribute("value"));
+
+//        driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+//        Assert.assertEquals("pizza", driver.findElement(By.id("elementosForm:comidaFavorita:2")).getAttribute("value"));
+
+        driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+        //Assert.assertEquals("vegetariano", driver.findElement(By.id("elementosForm:comidaFavorita:3")).getAttribute("value"));
+
+        //driver.findElements(By.id("elementosForm:escolaridade") List("2grauincomp")
+
+        driver.quit();
 
     }
 
